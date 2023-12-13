cf6d334762431ef297a9a59403a3941cdc169138
Miks Ramāns <miks.ramans@sorsera.com>
2023-12-13 22:20:45 +0200

    day13 part2

--- bin/day13.ml
+++ bin/day13.ml
@@ -24,37 +24,86 @@ let patterns =
   aux [] [] lines
 ;;
 
-let find_mirror_opt (pattern : string list) =
-  let rec aux n = function
-    | [], [] | [], _ | _, [] -> Some n
-    | hd1 :: tl1, hd2 :: tl2 -> if String.equal hd1 hd2 then aux n (tl1, tl2) else None
-  in
-  let rec aux2 n = function
-    | [], [] | [], _ | _, [] -> None
-    | hd1 :: tl1, hd2 :: tl2 ->
-      (match aux n (hd1 :: tl1, hd2 :: tl2) with
-       | Some n -> Some n
-       | None -> aux2 (n + 1) (hd2 :: hd1 :: tl1, tl2))
-  in
-  aux2 1 ([ List.hd pattern ], List.tl pattern)
-;;
-
 let rotate_pattern pattern =
   Util.rotate (pattern |> List.map (fun s -> s |> String.to_seq |> List.of_seq))
   |> List.map (fun l -> l |> List.to_seq |> String.of_seq)
 ;;
 
-let part1 patterns =
-  List.map
-    (fun pattern ->
-      match find_mirror_opt pattern with
-      | Some n -> n * 100
-      | None ->
-        (match find_mirror_opt (rotate_pattern pattern) with
-         | Some n -> n
-         | None -> 0))
-    patterns
-  |> List.fold_left ( + ) 0
+type reflection =
+  | Row of int
+  | Col of int
+
+let find_reflections (pattern : string list) =
+  let rec is_reflection = function
+    | [], [] | [], _ | _, [] -> true
+    | hd1 :: tl1, hd2 :: tl2 ->
+      if String.equal hd1 hd2 then is_reflection (tl1, tl2) else false
+  in
+  let rec aux2 acc = function
+    | [], [] | _, [] -> acc
+    | [], hd2 :: tl2 -> aux2 acc ([ hd2 ], tl2)
+    | hd1 :: tl1, hd2 :: tl2 ->
+      let acc =
+        if is_reflection (hd1 :: tl1, hd2 :: tl2)
+        then (List.length tl1 + 1) :: acc
+        else acc
+      in
+      aux2 acc (hd2 :: hd1 :: tl1, tl2)
+  in
+  let rows = aux2 [] ([], pattern) |> List.map (fun x -> Row x) in
+  let rotated = rotate_pattern pattern in
+  let cols = aux2 [] ([], rotated) |> List.map (fun x -> Col x) in
+  rows @ cols
 ;;
 
-let run () = print_endline @@ "Part 1: " ^ string_of_int (part1 patterns)
+let get_pattern_size pattern = List.length pattern * String.length (List.hd pattern)
+
+let flip_tile n pattern =
+  let xlen = List.hd pattern |> String.length in
+  let x, y = n mod xlen, n / xlen in
+  List.mapi
+    (fun i s ->
+      if i = y
+      then String.mapi (fun j c -> if j = x then if c = '#' then '.' else '#' else c) s
+      else s)
+    pattern
+;;
+
+let part1 patterns =
+  List.fold_left
+    (fun acc pattern ->
+      acc
+      +
+      match find_reflections pattern |> List.hd with
+      | Row n -> n * 100
+      | Col n -> n)
+    0
+    patterns
+;;
+
+let part2 patterns =
+  let reflections = List.map (fun p -> find_reflections p |> List.hd) patterns in
+  List.fold_left2
+    (fun acc pattern reflection ->
+      let next =
+        Array.init (get_pattern_size pattern) (fun i ->
+          pattern |> flip_tile i |> find_reflections)
+        |> Array.to_list
+        |> List.flatten
+        |> List.filter (( <> ) reflection)
+        |> List.hd
+      in
+      acc
+      +
+      match next with
+      | Row n -> n * 100
+      | Col n -> n)
+    0
+    patterns
+    reflections
+;;
+
+let run () =
+  print_endline @@ "Part 1: " ^ string_of_int (part1 patterns);
+  print_endline @@ "Part 2: " ^ string_of_int (part2 patterns)
+;;

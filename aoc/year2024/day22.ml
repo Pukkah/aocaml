20e1f0b158bf3349ce4d2c8270f486264e8a622f
Miks Ramāns <miksramans@gmail.com>
2024-12-23 17:10:56 +0200

    2024 day22 part2

--- aoc/year2024/day22.ml
+++ aoc/year2024/day22.ml
@@ -9,7 +9,7 @@ let next secret =
   secret
 ;;
 
-let rec calculate_nth_value ~f current ~n =
+let rec calculate_nth_value ~f ~n current =
   if n = 0 then current else calculate_nth_value ~f (f current) ~n:(n - 1)
 ;;
 
@@ -18,6 +18,60 @@ let part1 input =
   Base.List.sum (module Base.Int) ~f:(calculate_nth_value ~f:next ~n:2000) seeds
 ;;
 
-let test = "1\n10\n100\n2024"
-let%test _ = part1 test = 37327623
-let run ?(input = test) () = part1 input |> print_int |> print_newline
+module SMap = Map.Make (String)
+
+let make_key a b c d = Printf.sprintf "%d,%d,%d,%d" a b c d
+let ones_digit v = v mod 10
+
+let merge_maps maps =
+  List.fold_left
+    (SMap.merge (fun _ value1 value2 ->
+       match value1, value2 with
+       | Some v, None | None, Some v -> Some v
+       | Some v1, Some v2 -> Some (v1 + v2)
+       | _ -> None))
+    SMap.empty
+    maps
+;;
+
+let part2 input =
+  let seeds = parse input in
+  let aux seed =
+    let seq_map = ref SMap.empty in
+    let rec loop v n seq =
+      let nv = next v in
+      let bananas = ones_digit nv in
+      let diff = bananas - ones_digit v in
+      let seq = diff :: seq in
+      (match seq with
+       | a :: b :: c :: d :: _ ->
+         let key = make_key a b c d in
+         if not @@ SMap.mem key !seq_map then seq_map := SMap.add key bananas !seq_map
+       | _ -> ());
+      if n > 1 then loop nv (n - 1) seq
+    in
+    loop seed 2000 [];
+    !seq_map
+  in
+  List.map aux seeds
+  |> merge_maps
+  |> SMap.bindings
+  |> List.sort (fun (_, v1) (_, v2) -> compare v2 v1)
+  |> List.hd
+  |> snd
+;;
+
+let test1 = "1\n10\n100\n2024"
+let%test _ = part1 test1 = 37327623
+let test2 = "1\n2\n3\n2024"
+let%test _ = part2 test2 = 23
+
+let run ?input () =
+  let input1, input2 =
+    match input with
+    | Some s -> s, s
+    | None -> test1, test2
+  in
+  part1 input1 |> print_int |> print_newline;
+  part2 input2 |> print_int |> print_newline
+;;
